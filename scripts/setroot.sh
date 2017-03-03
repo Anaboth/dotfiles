dd977a58b5e2877e1214a52579441bf4bddac6fd
Root Sn0w <sn0wtoor@gmail.com>
2017-03-02 21:08:04 -0300

    Added setroot.sh

---
+++ scripts/setroot.sh
@@ -0,0 +1,26 @@
+mem () {
+	used=$(free -m | grep Mem | awk '{print $3 + $5}')
+
+	total=$(free -m | grep Mem | awk '{print $2}')
+
+	echo RAM $used MiB / $total MiB
+}
+
+up () {
+	_up=$(awk '{printf("%d:%d:%02d:%02d",($1/60/60/24),($1/60/60/24/7),($1/60/60%24),($1/60%60))}' /proc/uptime)
+
+	echo UP $_up
+}
+
+packages () {
+	qnt=$(pacman -Qq | wc -l)
+	upts=$(checkupdates | wc -l)
+	#upts="0"
+
+	echo Packages $qnt/$upts
+}
+
+while true; do
+xsetroot -name "$(mem) :: $(up) :: $(packages) :: $(date) :: An it harm none, do what ye will" ;
+sleep 1;
+done &
